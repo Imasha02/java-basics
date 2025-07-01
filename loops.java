2e03f7d04220c1aeb20f7a22ee55755a9e88fc8a
Imasha Buddhini <120939837+Imasha02@users.noreply.github.com>
2025-07-01 22:39:53 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,18 @@
+public class loops {
+    public static void main(String[] args) {
+        /*int i = 1;
+        while (i<=10) {
+            System.out.println("Hello");
+            i++; //i = i+1
+            
+        }*/
+
+        int i = 10;
+        while (i<=20) {
+            System.out.println(i);
+            i=i+2;
+        }
+
+    }
+    
+}
